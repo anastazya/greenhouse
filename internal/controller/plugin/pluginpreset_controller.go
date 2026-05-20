e7e357961a0ffbc8daec7058dbcb0103f2e756b5
Ivo Gosemann <ivo.gosemann@sap.com>
2026-05-20 11:54:59 +0200

    refactor(pluginpreset): create a dedicated PluginSpec
    Some fields (ClusterName, WaitFor) of the PluginSpec are not used by the PluginPreset
    With the upcoming changes of moving expression evaluation to the PluginPreset controller this will also effect the PluginOptionValues.
    
    This change brings a dedicated PluginSpec for PluginPresets and prepares the types for PluginOptionValues. The usage of the new PluginPresetPluginOptionValue will be implemented separately.
    Fields such as ClusterName, WaitFor are removed, both from types and webhook validation.
    
    Signed-off-by: Ivo Gosemann <ivo.gosemann@sap.com>
    Signed-off-by: Klaudiusz Fabryczny <klaudiusz.fabryczny@sap.com>

--- internal/controller/plugin/pluginpreset_controller.go
+++ internal/controller/plugin/pluginpreset_controller.go
@@ -502,6 +502,7 @@ func pluginSpecFromPluginPreset(preset *greenhousev1alpha1.PluginPreset, cluster
 		DisplayName:         preset.Spec.Plugin.DisplayName,
 		OptionValues:        preset.Spec.Plugin.OptionValues,
 		ReleaseNamespace:    preset.Spec.Plugin.ReleaseNamespace,
+		ReleaseName:         preset.Spec.Plugin.ReleaseName,
 		DeletionPolicy:      preset.Spec.Plugin.DeletionPolicy,
 		IgnoreDifferences:   preset.Spec.Plugin.IgnoreDifferences,
 		// Set the cluster name to the name of the cluster. The PluginSpec contained in the PluginPreset does not have a cluster name.
