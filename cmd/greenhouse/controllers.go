71c378504ea564d9244798e25cde24ca82c17b86
Klaudiusz Fabryczny <klaudiusz.fabryczny@sap.com>
2026-05-20 13:08:35 +0200

    feat(pluginpresets): add feature flag for pluginPreset
    Signed-off-by: Klaudiusz Fabryczny <klaudiusz.fabryczny@sap.com>

--- cmd/greenhouse/controllers.go
+++ cmd/greenhouse/controllers.go
@@ -35,7 +35,7 @@ var knownControllers = map[string]func(controllerName string, mgr ctrl.Manager) 
 
 	// Plugin controllers.
 	"plugin":       startPluginReconciler,
-	"pluginPreset": (&plugincontrollers.PluginPresetReconciler{}).SetupWithManager,
+	"pluginPreset": startPluginPresetReconciler,
 
 	"catalog":                 startCatalogReconciler,
 	"pluginDefinition":        startPluginDefinitionReconciler,
@@ -88,6 +88,13 @@ func startPluginReconciler(name string, mgr ctrl.Manager) error {
 		ExpressionEvaluationEnabled: featureFlags.IsExpressionEvaluationEnabled(),
 		IntegrationEnabled:          featureFlags.IsIntegrationEnabled(),
 		OCIMirroringEnabled:         featureFlags.IsOCIMirroringEnabled(),
+	}).SetupWithManager(name, mgr)
+}
+
+func startPluginPresetReconciler(name string, mgr ctrl.Manager) error {
+	return (&plugincontrollers.PluginPresetReconciler{
+		ExpressionEvaluationEnabled: featureFlags.IsPresetExpressionEvaluationEnabled(),
+		IntegrationEnabled:          featureFlags.IsPresetIntegrationEnabled(),
 	}).SetupWithManager(name, mgr)
 }
 
